605c1a8eecbdb5755a5f14d04eb8a4efa58fb9bf
Alexander Latyshev <imposya0@gmail.com>
2022-08-24 14:43:50 +0300

    2 constructors add + add with index

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyArrayList<E> {
     private static final int DEFAULT_CAPACITY = 10;
     private E[] elements;
@@ -5,20 +7,54 @@ public class MyArrayList<E> {
     private int size;
 
     public MyArrayList() {
-
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
     }
 
     public MyArrayList(int capacity) {
-
+        if (capacity > 0) {
+            elements = (E[]) new Object[capacity];
+            this.capacity = capacity;
+        } else if (capacity == 0) {
+            elements = (E[]) new Object[]{};
+            this.capacity = capacity;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public boolean add(E e) {
-        //TODO
-        return true;
+    public void add(E e) {
+        increaseCapacityIfNeeded();
+        elements[size] = e;
+        size++;
     }
 
     public void add(E element, int index) {
-        //TODO
+        boundsCheck(index);
+        increaseCapacityIfNeeded();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    private void increaseCapacityIfNeeded() {
+        if (size == elements.length) {
+            capacity = (int) (elements.length * 1.5 + 1);
+            elements = Arrays.copyOf(elements, (int) (elements.length * 1.5 + 1));
+        }
+    }
+
+    private void boundsCheck(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException();
+    }
+
+    public E get(int index) {
+        return elements[index];
+    }
+
+    public int size() {
+        return size;
     }
 
 
