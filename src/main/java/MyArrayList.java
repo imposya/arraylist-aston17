e0dc8702d857b7f1b0e3bd46b59676a26725f648
Alexander Latyshev <imposya0@gmail.com>
2022-08-24 14:39:10 +0300

    Initial commit

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,32 @@
+public class MyArrayList<E> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private E[] elements;
+    private int capacity;
+    private int size;
+
+    public MyArrayList() {
+
+    }
+
+    public MyArrayList(int capacity) {
+
+    }
+
+    public boolean add(E e) {
+        //TODO
+        return true;
+    }
+
+    public void add(E element, int index) {
+        //TODO
+    }
+
+
+    public void delete(E element) {
+        //TODO
+    }
+
+    public void sort() {
+        //TODO
+    }
+}
